e80deb13b5d0216cd8b2095e337a94a02de334e1
Marcos <marcosledezmacuevas@gmail.com>
2020-07-13 16:55:28 -0400

    Primer commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Marcos',
+
+    });
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola Data');
+    res.send(salida);
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}...`)
+});
